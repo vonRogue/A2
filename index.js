52aa4936271759584e80b98d43e9daca20d69fb8
Corey <>
2021-06-07 02:41:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const input = require(`readline-sync`);
+
+let candidate = input.question("Enter your name:"); 
+console.log ("Hello, " + candidate + "!");
+
+//let candidateAnswer = input.question("Who was the first American Woman in Space?",{
+
+ // trueValue: ['Sally Ride']
+//});
+//if(candidateAnswer === true)
+//{
+  //console.log('Correct');
+//} else if (candidateAnswer !== true) {
+  //console.log('Incorrect')};
+  function askQuestion() 
+    for (let i=0; i < questions.length; i++)
+    candidateAnswers[i] =
+
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS?"]
+let correctAnswers = ["Sally Ride", true, 40, "Trajectory", 3]
